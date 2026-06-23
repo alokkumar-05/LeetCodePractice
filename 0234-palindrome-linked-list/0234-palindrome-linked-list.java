da0d05e1cfa044625078041e0ad4a3ee951e5140
Alok Kumar <alokkumarswn01@gmail.com>
2026-06-23 05:34:41 +0530

    Time: 11 ms (13.9%), Space: 70.1 MB (99.99%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,47 +9,19 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        //edge case
-        if(head==null || head.next==null){
+    ListNode curr;
+    public boolean solve(ListNode head){
+        if(head==null){
             return true;
         }
-
-        //find middle
-        ListNode slow=head;
-        ListNode fast=head;
-        ListNode prev= null;
-
-        while(fast!=null && fast.next!=null){
-            prev=slow;
-            slow = slow.next;
-            fast=fast.next.next;
+        if(!solve(head.next)|| head.val!=curr.val){
+            return false;
         }
-
-        //reverse second half
-        ListNode revHead=reverseList(slow);
-        prev.next=null;
-
-        //compare both half
-        while(revHead!=null && head!=null){
-            if(revHead.val!=head.val){
-                return false;
-            }
-            revHead=revHead.next;
-            head=head.next;
-        }
-        return true;       
+        curr=curr.next;
+        return true;
     }
-    
-    //recursive reverse
-    public ListNode reverseList(ListNode head){
-        if(head==null || head.next==null){
-            return head;
-        }
-        ListNode last = reverseList(head.next);
-        head.next.next=head;
-        head.next=null;
-        return last;
+    public boolean isPalindrome(ListNode head) {
+        curr =head;
+        return solve(head);
     }
-
 }

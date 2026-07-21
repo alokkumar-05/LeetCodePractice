f844b9ab3c111e1e1cf96497ee34c230c6c4324d
Alok Kumar <alokkumarswn01@gmail.com>
2026-07-22 03:57:41 +0530

    Time: 28 ms (22.62%), Space: 64.2 MB (17.96%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,45 +1,47 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        int V = graph.length;
+
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int n = graph.length;
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < V; i++)
             adj.add(new ArrayList<>());
-        }
-        int[] outDegree = new int[n];
-        for (int i = 0; i < n; i++) {
-            outDegree[i] = graph[i].length;
 
-            for (int nei : graph[i]) {
-                adj.get(nei).add(i);
+        // Reverse graph
+        for (int u = 0; u < V; u++) {
+            for (int v : graph[u]) {
+                adj.get(v).add(u);
             }
         }
+
+        int[] outDegree = new int[V];
+        for(int i = 0 ; i<V;i++){
+            for(int edges : adj.get(i)){
+                outDegree[edges]++;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
         Queue<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            if (outDegree[i] == 0) {
-                q.offer(i);
+
+        for(int i = 0; i<V;i++){
+            if(outDegree[i]==0){
+                res.add(i);
+                q.add(i);
             }
         }
-        boolean[] safe = new boolean[n];
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            safe[node] = true;
 
-            for (int parent : adj.get(node)) {
-                outDegree[parent]--;
-                if (outDegree[parent] == 0) {
-                    q.offer(parent);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            for(int nei : adj.get(curr)){
+                outDegree[nei]--;
+                if(outDegree[nei]==0){
+                    res.add(nei);
+                    q.offer(nei);
                 }
             }
         }
 
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (safe[i]) {
-                res.add(i);
-            }
-        }
-        Collections.sort(res);
-        return res;
+     Collections.sort(res);
+     return res;
+
     }
 }

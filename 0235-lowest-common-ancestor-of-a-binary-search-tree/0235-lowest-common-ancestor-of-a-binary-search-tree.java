2335fe6a4607e875dcb70c15166333b63d9c1729
Alok Kumar <alokkumarswn01@gmail.com>
2026-09-18 01:17:59 +0530

    Time: 6 ms (96.98%), Space: 47.7 MB (54.49%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,39 +10,20 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> l1= new ArrayList<>();
-        List<TreeNode> l2= new ArrayList<>();
+        if (root == null) {
+            return null;
+        }
+        
+        if(root==p || root==q){
+            return root;
+        }
 
-        helper(root,l1,p);
-        helper(root,l2,q);
-
-        TreeNode ans = null;
-        int n = Math.min(l1.size(), l2.size());
-        for(int i=0 ; i<n;i++){
-            if(l1.get(i)==l2.get(i)){
-               ans=l1.get(i);
-            }
-            else{
-                break;
-            }
+        if(p.val>root.val && q.val > root.val){
+           return lowestCommonAncestor(root.right , p , q);
         }
-        return ans;
-    }
-    private boolean  helper(TreeNode root , List<TreeNode> list, TreeNode tar){
-        if(root==null){
-            return false;
+        if(p.val<root.val && q.val < root.val){
+           return lowestCommonAncestor(root.left , p , q);
         }
-        list.add(root);
-        if(root==tar){
-            return true;
-        }
-        boolean left = helper(root.left, list , tar);
-        boolean right = helper(root.right, list , tar);
-
-        if(left || right){
-            return true;
-        }
-        list.remove(list.size()-1);
-        return false;
+        return root;
     }
 }
